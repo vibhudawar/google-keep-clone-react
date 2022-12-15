be7c9ad5db4b71b3ca105d75fce55297b7d1804f
vibhudawar <9vibhu@gmail.com>
2022-12-15 19:10:43 +0530

    pushing to github

---
+++ build/precache-manifest.dd878acdb06014e72e7c92c55e2b5b10.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55426d87a2f90b6c50735b8853e77e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28590cc89be37effedf3",
+    "url": "/static/js/2.e191c4e3.chunk.js"
+  },
+  {
+    "revision": "8c50b8e16f765d9d8c63",
+    "url": "/static/js/main.1294f648.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
